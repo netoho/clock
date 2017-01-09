db926db8fab9852b13a2ccd2d833968289b7bc68
neto <hatreun@gmail.com>
2017-01-09 00:05:30 -0600

    adding colon

--- app/services/server-date.js
+++ app/services/server-date.js
@@ -3,7 +3,7 @@ import ENV from 'clock/config/environment';
 
 export default Ember.Service.extend({
   getDate(){
-    let url = `${ENV.APP.API.protocol}://${ENV.APP.API.domain}:${ENV.APP.API.port}/${ENV.APP.API.endpoint}`
+    let url = `${ENV.APP.API.protocol}://${ENV.APP.API.domain}:${ENV.APP.API.port}/${ENV.APP.API.endpoint}`;
     return Ember.$.getJSON(url);
   }
 });
