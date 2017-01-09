7a84c0ae979eeeb2e7c2dbec1b2b8fa58fa602ce
neto <hatreun@gmail.com>
2017-01-09 00:03:02 -0600

    importing directly socket io

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,8 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('vendor/socket.io-1.4.5.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
