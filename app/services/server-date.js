754311f95c02d080248dd4b5637a0ff9811ddbbe
neto <hatreun@gmail.com>
2017-01-09 00:02:35 -0600

    Adding the service to query the current time

---
+++ app/services/server-date.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+import ENV from 'clock/config/environment';
+
+export default Ember.Service.extend({
+  getDate(){
+    let url = `${ENV.APP.API.protocol}://${ENV.APP.API.domain}:${ENV.APP.API.port}/${ENV.APP.API.endpoint}`
+    return Ember.$.getJSON(url);
+  }
+});
---
+++ tests/unit/services/server-date-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('service:server-date', 'Unit | Service | server date', {
+  // Specify the other units that are required for this test.
+  // needs: ['service:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let service = this.subject();
+  assert.ok(service);
+});
